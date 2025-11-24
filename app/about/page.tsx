90d7f957f980b859a51ed2c01adc25573aaf8535
Wonho Bae <77373106+lyh4215@users.noreply.github.com>
2025-11-24 08:48:43 +0000

    feat : initial commit

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+        <div>
+        <h1>
+            this is about you!
+        </h1>
+        </div>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">Hello Codespaces ✨</h1>
+    </main>
+  );
+}
